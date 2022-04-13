cb35352ca4e1c7daacd7d66a413521b87142ec64
PERFICIENT\david.henaoc <david.henaoc@perficient.com>
2022-04-13 09:45:02 -0500

    Primer Commit de la rama de UI

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,24 +1,29 @@
 function fn() {
   var env = karate.env; // get system property 'karate.env'
-  karate.log('karate.env system property was:', env);
+
+  karate.log("karate.env system property was:", env);
+
   if (!env) {
-    env = 'dev';
+    env = "dev";
   }
 
-  
   var config = {
-     apiUrl: 'https://gorest.co.in/public/v2/'
-  }
-  if (env == 'dev') {
+    apiUrl: "https://gorest.co.in/public/v2/",
+  };
+
+  if (env == "dev") {
     // customize
     // e.g. config.foo = 'bar';
-  } else if (env == 'e2e')
-  {
- 
+  } else if (env == "e2e") {
   }
-  var accessToken = "515affa5e7a547730508144f734fd2bbee8110efa67fb4afbcc1701e59ba3dec"     
-  karate.configure('headers', {Authorization: 'Bearer '+ accessToken, Accept: 'application/json;charset=utf-8'})
-  karate.configure('logPrettyRequest', true)
-  karate.configure('logPrettyResponse', true)
+
+  var accessToken =
+    "515affa5e7a547730508144f734fd2bbee8110efa67fb4afbcc1701e59ba3dec";
+  karate.configure("headers", {
+    Authorization: "Bearer " + accessToken,
+    Accept: "application/json;charset=utf-8",
+  });
+  karate.configure("logPrettyRequest", true);
+  karate.configure("logPrettyResponse", true);
   return config;
 }
